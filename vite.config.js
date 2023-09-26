13214dfadf155478bb7f300810875c3f0ad08506
Mehrdad Mehralian <dadwic0@gmail.com>
2023-09-26 13:59:53 +0300

    feat: add pwa assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'rialir',
+        short_name: 'rialir',
+        description: 'Calculate Shipping App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-1024x1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
