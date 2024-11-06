792df9497cab096b201a8bfd7edc5ddb4e271e11
Mehrdad Mehralian <dadwic@yahoo.com>
2024-11-06 15:41:27 +0300

    update pwa manifest icons

--- vite.config.js
+++ vite.config.js
@@ -7,27 +7,27 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'rialir',
         short_name: 'rialir',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         icons: [
+          { src: '/favicon.ico', type: 'image/x-icon', sizes: '16x16 32x32' },
+          { src: '/icon-192.png', type: 'image/png', sizes: '192x192' },
+          { src: '/icon-512.png', type: 'image/png', sizes: '512x512' },
           {
-            src: 'pwa-192x192.png',
+            src: '/icon-192-maskable.png',
+            type: 'image/png',
             sizes: '192x192',
-            type: 'image/png',
+            purpose: 'maskable',
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/icon-512-maskable.png',
+            type: 'image/png',
             sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-1024x1024.png',
-            sizes: '1024x1024',
-            type: 'image/png',
+            purpose: 'maskable',
           },
         ],
       },
